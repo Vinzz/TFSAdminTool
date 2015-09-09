ff6b78252a02b45a38f775f81e5fbedab930daac
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 15:03:22 +0200

    fix

--- LivraisonScript.ps1
+++ LivraisonScript.ps1
@@ -1,16 +1,18 @@
 ﻿'Begin post build script'
 
+'env var: ' + $Env:TF_BUILD_BINARIESDIRECTORY
+
 # Extract current version
-$version = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
+#$version = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
 
 # Get installer Name
-$Name = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
+#$Name = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
 
 
 # Compute new name
-$NewName = $Name.Replace('msi', $version + '.msi')
+#$NewName = $Name.Replace('msi', $version + '.msi')
 
 # Rename
-Rename-Item -NewName $NewName -Path $Name
+#Rename-Item -NewName $NewName -Path $Name
 
 'End post build script'
