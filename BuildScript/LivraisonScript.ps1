6873a4f53f4d7d17abeba13b2cb19bf9119a8311
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 15:23:44 +0200

    works on my box

--- BuildScript/LivraisonScript.ps1
+++ BuildScript/LivraisonScript.ps1
@@ -1,16 +1,16 @@
 ﻿write-host 'Begin post build script'
 
-write-host 'env var: ' + $Env:TF_BUILD_BINARIESDIRECTORY
+write-host ('env var: ' + $env:TF_BUILD_BINARIESDIRECTORY)
 
 # Extract current version
-$version = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
+$version = (Get-Item ($env:TF_BUILD_BINARIESDIRECTORY + "\TFSAdministrationTool.exe")).VersionInfo.FileVersion
 
 # Get installer Name
-$Name = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
+$Name = (Get-Item ($env:TF_BUILD_BINARIESDIRECTORY + "\TFSAdministrationTool.msi")).FullName
 
 
 # Compute new name
-$NewName = $Name.Replace('msi', $version + '.msi')
+$NewName = $Name.Replace('msi', ($version + '.msi'))
 
 # Rename
 Rename-Item -NewName $NewName -Path $Name
