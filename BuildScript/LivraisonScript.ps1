7d84d3bbb74810c06d711bb72872d53eb3388623
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 15:14:49 +0200

    go

--- BuildScript/LivraisonScript.ps1
+++ BuildScript/LivraisonScript.ps1
@@ -3,16 +3,16 @@
 write-host 'env var: ' + $Env:TF_BUILD_BINARIESDIRECTORY
 
 # Extract current version
-#$version = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
+$version = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
 
 # Get installer Name
-#$Name = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
+$Name = (Get-Item $Env:TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
 
 
 # Compute new name
-#$NewName = $Name.Replace('msi', $version + '.msi')
+$NewName = $Name.Replace('msi', $version + '.msi')
 
 # Rename
-#Rename-Item -NewName $NewName -Path $Name
+Rename-Item -NewName $NewName -Path $Name
 
 write-host 'End post build script'
