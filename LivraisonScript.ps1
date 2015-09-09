c8920c08493d5523e80ebc53900cb6fb5fd92a8a
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 14:43:40 +0200

    WIP script

---
+++ LivraisonScript.ps1
@@ -0,0 +1,13 @@
+﻿
+# Extract current version
+$version = (Get-Item $Env: TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.exe").VersionInfo.FileVersion
+
+# Get installer Name
+$Name = (Get-Item $Env: TF_BUILD_BINARIESDIRECTORY + "TFSAdministrationTool.msi").Name
+
+
+# Compute new name
+$NewName = $Name.Replace('msi', $version + '.msi')
+
+# Rename
+Rename-Item -NewName $NewName -Path $Name
