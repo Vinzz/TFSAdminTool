4e53d4297002bf7d6cda71022e97f7989d9dc886
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 14:34:03 +0200

    Add liv script

---
+++ BuildScript/LivraisonScript.ps1
@@ -0,0 +1,13 @@
+﻿
+# Extract current version
+$version = (Get-Item "TFSAdministrationTool.exe").VersionInfo.FileVersion
+
+# Get installer Name
+$Name = (Get-Item "TFSAdministrationTool.msi").Name
+
+
+# Compute new name
+$Name = $Name.Replace('msi', $version + '.msi')
+
+# Rename
+Rename-Item -NewName $Name -Path "C:\Users\Vinzz\AppData\Local\Temp\TFSAdministrationTool.msi"
