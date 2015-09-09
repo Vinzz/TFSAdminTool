cbc220501c0899d9dff98a45f5ee87b08333d559
Vincent Tollu <vincent.tollu@orange.com>
2015-09-09 15:36:05 +0200

    now copy installer in install directory

--- BuildScript/LivraisonScript.ps1
+++ BuildScript/LivraisonScript.ps1
@@ -15,4 +15,12 @@ $NewName = $Name.Replace('msi', ($version + '.msi'))
 # Rename
 Rename-Item -NewName $NewName -Path $Name
 
+$InstallFolder = "\\vtfs-drop01.ad-build.local\Install\TFS Administration Tool OAB\"
+
+#remove old version, if any
+GET-CHILDITEM $InstallFolder –recurse –include *.msi | REMOVE-ITEM
+
+# Copy
+Copy-Item $NewName -Destination $InstallFolder
+
 write-host 'End post build script'
